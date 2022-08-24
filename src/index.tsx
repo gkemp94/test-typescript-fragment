76f0b988e9d6e42a907b082d4903495683dc264f
George Kemp <georgekemp94@gmail.com>
2022-08-24 11:21:13 -0600

    Add Description of Errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import React from "react";
 
 const test = () => "asd";
 
+// No Errors
 const jsxWithJsxFragment = <>{test}</>;
+
+// Type '() => string' is not assignable to type 'ReactNode'.
 const jsxWithReactFragment = <React.Fragment>{test}</React.Fragment>;
